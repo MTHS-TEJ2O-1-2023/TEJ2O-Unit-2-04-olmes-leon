58cc1c5c94a4589b94814719ee4c61ea307dcceb
olmes-leon <144264100+olmes-leon@users.noreply.github.com>
2024-01-07 17:47:48 -0500

    fixed TypeScript

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: olmes 
+ * Created by: olmes
  * Created on: Sep 2023
  * This program ...
 */
@@ -11,12 +11,12 @@ basic.clearScreen()
 basic.showIcon(IconNames.Happy)
 
 input.onButtonPressed(Button.A, function () {
-    randomNumber = randint(1, 6)
-    basic.showNumber(randomNumber)
+  randomNumber = randint(1, 6)
+  basic.showNumber(randomNumber)
 })
 
 basic.clearScreen()
 
-input.onButtonPressed(Button.B, function() {
-    input.temperature()
+input.onButtonPressed(Button.B, function () {
+  input.temperature()
 })
